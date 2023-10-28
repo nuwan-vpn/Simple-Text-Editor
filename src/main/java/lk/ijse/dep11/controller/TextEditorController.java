969187d618f1cb077d3a75427055849948633b29
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2023-10-28 21:18:21 +0530

    Complete TextEditorController.java file

--- src/main/java/lk/ijse/dep11/controller/TextEditorController.java
+++ src/main/java/lk/ijse/dep11/controller/TextEditorController.java
@@ -1,8 +1,64 @@
 package lk.ijse.dep11.controller;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
 import javafx.scene.web.HTMLEditor;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class TextEditorController {
-
     public HTMLEditor txtEditor;
+    public MenuItem mnuNew;
+    public MenuItem mnuSave;
+    public MenuItem mnuOpen;
+    public MenuItem mnuPrint;
+    public MenuItem mnuClose;
+    public MenuItem mnuCopy;
+    public MenuItem mnuPaste;
+    public MenuItem mnuCut;
+    public MenuItem mnuDeleteAll;
+    public MenuItem mnuAbout;
+
+    public void initialize(){
+        mnuNew.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                txtEditor.setHtmlText("");
+            }
+        });
+
+        mnuClose.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Platform.exit();
+            }
+        });
+
+        mnuAbout.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try {
+                    URL resource =this.getClass().getResource("/view/AboutForm.fxml");
+                    Parent container = FXMLLoader.load(resource);
+                    Scene scene = new Scene(container);
+                    Stage stage = new Stage();
+                    stage.setScene(scene);
+                    stage.setTitle("About");
+                    stage   .initModality(Modality.APPLICATION_MODAL);
+                    stage.show();
+                    stage.centerOnScreen();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
 }
